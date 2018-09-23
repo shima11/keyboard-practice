beeea986b07e7c640acb37027d2260810ec8d693
Shima <shima.jin@icloud.com>
2018-09-23 17:10:51 +0900

    Initial Commit

---
+++ keyboard-practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  keyboard-practice
+//
+//  Created by Jinsei Shima on 2018/09/23.
+//  Copyright © 2018 Jinsei Shima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
