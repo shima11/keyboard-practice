85fd377878b6c4aa5fc6b8df387b46194cff5c5e
Shima <shima.jin@icloud.com>
2018-09-26 01:36:10 +0900

    update

--- keyboard-practice/ViewController.swift
+++ keyboard-practice/ViewController.swift
@@ -6,27 +6,112 @@
 //  Copyright © 2018 Jinsei Shima. All rights reserved.
 //
 
-// https://dev.classmethod.jp/smartphone/ios-uiwindow/
-
 import UIKit
+
 import EasyPeasy
+import RxSwift
+import RxKeyboard
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var textField: UITextField!
+    var scrollView: UIScrollView = .init()
 
-    @IBOutlet weak var inputContainerView: UIView!
+    var textField: UITextField = .init()
+    var sendButton: UIButton = .init()
+    var inputContainerView: UIView = .init()
 
     private var keyboardWindow: UIWindow?
+
+    private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.backgroundColor = .white
+
 //        textField.resignFirstResponder()
 //        textField.becomeFirstResponder()
 
+        view.addSubview(scrollView)
+        view.addSubview(inputContainerView)
+
+        inputContainerView.addSubview(textField)
+        inputContainerView.addSubview(sendButton)
+
         scrollView.keyboardDismissMode = .interactive
+
+        inputContainerView.backgroundColor = .groupTableViewBackground
+        sendButton.setTitle("Send", for: .normal)
+        sendButton.setAttributedTitle(
+            NSAttributedString(
+                string: "Send",
+                attributes: [
+                    NSAttributedStringKey.font: UIFont.systemFont(ofSize: 14, weight: .bold),
+                    NSAttributedStringKey.foregroundColor: UIColor.darkGray
+                ]
+            ),
+            for: .normal
+        )
+        textField.backgroundColor = .white
+        textField.borderStyle = .none
+
+
+        scrollView.easy.layout(
+            Top(),
+            Left(),
+            Right()
+        )
+
+        inputContainerView.easy.layout(
+            Top().to(scrollView, .bottom),
+            Left(),
+            Right(),
+            Bottom().with(.low)
+        )
+
+        textField.easy.layout(
+            Top(8),
+            Left(8),
+            Bottom(8)
+        )
+
+        sendButton.easy.layout(
+            CenterY().to(textField),
+            Left(8).to(textField, .right),
+            Right(8)
+        )
+
+
+        RxKeyboard.instance.visibleHeight
+            .drive(onNext: { [weak self] keyboardVisibleHeight in
+                guard let `self` = self else { return }
+                if #available(iOS 11.0, *) {
+                    self.inputContainerView.easy.layout(
+                        Bottom(keyboardVisibleHeight).to(self.view.safeAreaLayoutGuide, .bottom)
+                    )
+                } else {
+                    self.inputContainerView.easy.layout(
+                        Bottom(keyboardVisibleHeight).to(self.bottomLayoutGuide, .top)
+                    )
+                }
+                self.view.setNeedsLayout()
+                UIView.animate(withDuration: 0) {
+                    self.scrollView.contentInset.bottom = keyboardVisibleHeight + self.inputContainerView.bounds.height
+                    self.scrollView.scrollIndicatorInsets.bottom = self.scrollView.contentInset.bottom
+                    self.view.layoutIfNeeded()
+                }
+                print("content inset:", self.scrollView.contentInset)
+            })
+            .disposed(by: disposeBag)
+
+        RxKeyboard.instance.willShowVisibleHeight
+            .drive(onNext: { keyboardVisibleHeight in
+                self.scrollView.contentOffset.y += keyboardVisibleHeight
+                print("content offset y:", self.scrollView.contentInset)
+            })
+            .disposed(by: disposeBag)
+
+
 
         NotificationCenter
             .default
